e6d8df4dae959442a9697c2c7ffe5b7cd52f26e8
Jacob Osborne-Walker <jacob.owalker6@gmail.com>
2025-11-28 15:12:48 +0000

    Lock Pages
    Initial version of locked pages so that the user must login first

--- Pages/app.js
+++ Pages/app.js
@@ -1,5 +1,39 @@
-// basic js function
+
+// click login
+let ClickedLogin = false;
+
+function login() {
+   
+    // login attempted
+    ClickedLogin = true;
+
+    // unlocks sidebar
+    document.getElementById("sidebar").classList.remove("locked");
+
+    // dashboard
+    showPage("dashboard");
+}
+
+function logout() {
+    // reset
+    ClickedLogin = false;
+
+    // lock sidebar
+    document.getElementById("sidebar").classList.add("locked");
+
+    // return
+    showPage("login");
+}
+
+// show pages
 function showPage(pageId) {
+    
+    // block navigation
+    if (!ClickedLogin && pageId !== "login") {
+        alert("Please click the Login button first.");
+        return;
+    }
+
     const pages = document.querySelectorAll(".page");
 
     pages.forEach(page => {
