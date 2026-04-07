40de76da789a1de0ef5e2b8e14dcf1a5e709790e
Jacob Osborne-Walker <jacob.owalker6@gmail.com>
2026-04-07 17:36:36 +0100

    scanner info added

--- Node Code/pi_config.py
+++ Node Code/pi_config.py
@@ -4,9 +4,9 @@
 ## need to update when fix server
 import os
 
-SERVER_URL =  os.getenv("SERVER_URL", "put here when server working")
+SERVER_URL =  os.getenv("SERVER_URL", "https://unevaporated-conner-gewgawed.ngrok-free.dev/")
 SCANNER_UID = os.getenv("SCANNER_UID", "SCAN-001")
-API_KEY = os.getenv("API_KEY", "put the key here when i seed the db again")
+API_KEY = os.getenv("API_KEY", "56cdc2286309de6d338dfc22136a9cc2ea918321c43034697b8db9ba534c9bad")
 CHECKIN_INTERVALS = int(os.getenv("CHECKIN_INTERVALS", "60"))
 POLL_INTERVALS = int(os.getenv("POLL_INTERVALS", "30"))
 RESULTS_PATH = os.path.join(os.path.dirname(__file__), "faake_results.json")
