fc481c658a3f4fc35291d7058c26151143ce87ce
FantasyLemon <146332787+JacobOsborneWalker@users.noreply.github.com>
2026-04-07 18:25:33 +0100

    stash changes??

--- Node Code/pi_config.py
+++ Node Code/pi_config.py
@@ -1,12 +1,20 @@
 # pi config - configuration for the scanners
 
 
-## need to update when fix server
+
 import os
 
 SERVER_URL =  os.getenv("SERVER_URL", "https://unevaporated-conner-gewgawed.ngrok-free.dev")
+
+## scanners
 SCANNER_UID = os.getenv("SCANNER_UID", "SCAN-001")
+#SCANNER_UID = os.getenv("SCANNER_UID", "SCAN-002")
+#SCANNER_UID = os.getenv("SCANNER_UID", "SCAN-003")
+#SCANNER_UID = os.getenv("SCANNER_UID", "SCAN-004")
+
+
+## api key
 API_KEY = os.getenv("API_KEY", "56cdc2286309de6d338dfc22136a9cc2ea918321c43034697b8db9ba534c9bad")
-CHECKIN_INTERVAL = int(os.getenv("CHECKIN_INTERVAL", "60"))
-POLL_INTERVAL = int(os.getenv("POLL_INTERVAL", "30"))
+CHECKIN_INTERVALS = int(os.getenv("CHECKIN_INTERVALS", "60"))
+POLL_INTERVALS = int(os.getenv("POLL_INTERVALS", "30"))
 FAKE_RESULTS_PATH = os.path.join(os.path.dirname(__file__), "fake_results.json")
